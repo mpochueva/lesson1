a498e44b5db781cd567ed8c955a8766ecf17fa49
Mariia <mariia.pochueva@gmail.com>
2018-11-25 02:11:21 +0300

    ...

--- vat.py
+++ vat.py
@@ -1,7 +1,12 @@
 def get_vat(price, vat_rate):
-    vat = price/100 * vat_rate
-    price_no_vat = price - vat
-    print(price_no_vat)
+    try:
+        price = abs(float(price))
+        vat_rate = int(vat_rate)
+        vat = price/100 * vat_rate
+        price_no_vat = price - vat
+        print(price_no_vat)
+    except (TypeError, ValueError):
+        print("Неправильный формат данных")
 
 
 price1 = 100
@@ -12,6 +17,6 @@ price2 = 500
 vat_rate2 = 10
 get_vat(price2, vat_rate2)
 
-get_vat(50, '5')
+get_vat(50, "5")
 get_vat(-100, 18)
-
+get_vat("okr", 15)
