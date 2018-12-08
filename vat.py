6d2fe45181285f87ba31524ea82280493f96b418
Mariia <mariia.pochueva@gmail.com>
2018-12-08 17:13:26 +0300

    Try/except structure updated

--- vat.py
+++ vat.py
@@ -2,11 +2,12 @@ def get_vat(price, vat_rate):
     try:
         price = abs(float(price))
         vat_rate = int(vat_rate)
-        vat = price/100 * vat_rate
-        price_no_vat = price - vat
-        print(price_no_vat)
     except (TypeError, ValueError):
         print("Неправильный формат данных")
+        return()
+    vat = price/100 * vat_rate
+    price_no_vat = price - vat
+    print(price_no_vat)
 
 
 price1 = 100
